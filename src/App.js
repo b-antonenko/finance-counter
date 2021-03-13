ec1f719de1fa5f0634be45ce0cdf34177725a97c
b-antonenko <b_antonenko@icloud.com>
2021-03-13 20:47:22 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import React, { useContext } from "react"
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
+import SignUp from './components/SignUp'
+import Login from './components/Login'
+import Home from './components/Home'
+import { AuthProvider } from "./Auth";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <AuthProvider>
+            <Router>
+                <div>
+                    <Route exact path="/" component={Home} />
+                    <Route exact path="/login" component={Login} />
+                    <Route exact path="/signup" component={SignUp} />
+                </div>
+            </Router>
+        </AuthProvider>
+  )
 }
 
 export default App;
